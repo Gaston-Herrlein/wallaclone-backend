c5a8f70b02773261966f38e2c07554ed377f8e1f
Gaston Herrlein <herrleingaston@gmail.com>
2025-10-28 16:52:12 +0100

    feat: environment variables are added to connect to a remote Redis database

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,9 +1,19 @@
 import { createClient } from 'redis';
 
-const redisClient = createClient();
+console.log('Connecting to Redis server...');
+console.log('Host:', process.env.REDIS_SOCKET_HOST);
+console.log('Username:', process.env.REDIS_USERNAME ? 'Provided' : 'Not Provided');
+console.log('SecretKey:', process.env.REDIS_SECRETACCESSKEY ? '********' : 'Not Provided');
 
-redisClient.on('error', (err) => {
-	console.error('Redis client error', err);
-}).connect();
+const redisClient = createClient({
+  username: String(process.env.REDIS_USERNAME),
+  password: String(process.env.REDIS_SECRETACCESSKEY),
+  socket: {
+    host: String(process.env.REDIS_SOCKET_HOST),
+    port: Number(process.env.REDIS_SOCKET_PORT),
+  },
+});
+
+redisClient.on('error', (err) => console.log('Redis Client Error', err)).connect();
 
 export default redisClient;
